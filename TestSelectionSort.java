4fbc28948e9ab2791e65650904ee4064265974d0
Luca Irimie <lucaniri@umich.edu>
2023-04-13 18:05:40 -0400

    changes made to mixed and duplicate tests

--- TestSelectionSort.java
+++ TestSelectionSort.java
@@ -32,10 +32,10 @@ public void testPositive(){
 	Sortedarr[2] = 8;
 	Sortedarr[3] = 9;
 	Sortedarr[4] = 10;
-	
+
 	SelectionSort obj= new SelectionSort();
 	obj.basicSelectionSort(arr);
-	
+
 	assertArrayEquals("Not Equal", Sortedarr, obj.basicSelectionSort(arr));
 }
 public void testNegative(){
@@ -51,15 +51,15 @@ public void testNegative(){
 	Sortedarr[2] = -8;
 	Sortedarr[3] = -7;
 	Sortedarr[4] = -2;
-	
+
 	SelectionSort obj= new SelectionSort();
 	obj.basicSelectionSort(arr);
-	
+
 	assertArrayEquals("Not Equal", Sortedarr, obj.basicSelectionSort(arr));
 }
 public void testMixed(){
 	int[] arr = new int[5];
-	arr[0] = 8;
+	arr[0] = 4;
 	arr[1] = -9;
 	arr[2] = 7;
 	arr[3] = -10;
@@ -69,11 +69,11 @@ public void testMixed(){
 	Sortedarr[1] = -9;
 	Sortedarr[2] = 2;
 	Sortedarr[3] = 7;
-	Sortedarr[4] = 8;
-	
+	Sortedarr[4] = 4;
+
 	SelectionSort obj= new SelectionSort();
 	obj.basicSelectionSort(arr);
-	
+
 	assertArrayEquals("Not Equal", Sortedarr, obj.basicSelectionSort(arr));
 }
 public void testDuplicates(){
@@ -81,18 +81,20 @@ public void testDuplicates(){
 	arr[0] = 8;
 	arr[1] = 9;
 	arr[2] = 8;
-	arr[3] = 10;
+	arr[3] = 2;
 	arr[4] = 2;
+
 	int[] Sortedarr = new int[5];
+
 	Sortedarr[0] = 2;
-	Sortedarr[1] = 8;
+	Sortedarr[1] = 2;
 	Sortedarr[2] = 8;
-	Sortedarr[3] = 9;
-	Sortedarr[4] = 10;
-	
+	Sortedarr[3] = 8;
+	Sortedarr[4] = 9;
+
 	SelectionSort obj= new SelectionSort();
 	obj.basicSelectionSort(arr);
-	
+
 	assertArrayEquals("Not Equal", Sortedarr, obj.basicSelectionSort(arr));
 }
 }
